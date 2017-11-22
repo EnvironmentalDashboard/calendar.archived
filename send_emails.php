135992fc35c77154531071f5740648b0c48cec32
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2017-11-22 14:43:47 -0500

    emails work now

--- send_emails.php
+++ send_emails.php
@@ -1,17 +1,44 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '/var/www/PHPMailer/src/Exception.php';
+require '/var/www/PHPMailer/src/PHPMailer.php';
+require '/var/www/PHPMailer/src/SMTP.php';
+
+
 error_reporting(-1);
 ini_set('display_errors', 'On');
 date_default_timezone_set('America/New_York');
 chdir(__DIR__);
 require '../includes/db.php'; // Has $db
-foreach ($db->query('SELECT id, recipient, subject, txt_message, html_message FROM outbox') as $email) {
+foreach ($db->query('SELECT id, recipient, subject, html_message, txt_message FROM outbox') as $email) {
   if (filter_var($email['recipient'], FILTER_VALIDATE_EMAIL)) {
-    $hash = sha1(uniqid());
-    $headers = "MIME-Version: 1.0\r\nFrom: Environmental Dashboard <no-reply@oberlindashboard.org>\r\nContent-Type: multipart/alternative; boundary=\"{$hash}\"\r\n\r\n";
-    // See https://stackoverflow.com/a/10267876/2624391
-    if (mail($email['recipient'], $email['subject'], "{$email['txt_message']}\r\n\r\n--{$hash}\r\nContent-Type: text/html; charset=\"iso-8859-1\"\r\n\r\n{$email['html_message']}\r\n\r\n--{$hash}--")) {
-      $stmt = $db->prepare('DELETE FROM outbox WHERE id = ?');
-      $stmt->execute(array($email['id']));
+    $mail = new PHPMailer;
+    $mail->setFrom('no-reply@oberlindashboard.org', 'Environmental Dashboard');
+    $mail->addAddress($email['recipient']);
+    $mail->Subject = $email['subject'];
+    $mail->msgHTML($email['html_message']);
+    //This should be the same as the domain of your From address
+    $mail->DKIM_domain = 'oberlindashboard.org';
+    //See the DKIM_gen_keys.phps script for making a key pair -
+    //here we assume you've already done that.
+    //Path to your private key:
+    $mail->DKIM_private = '/etc/opendkim/keys/oberlindashboard.org/mail.private';
+    //Set this to your own selector
+    $mail->DKIM_selector = 'mail';
+    //Put your private key's passphrase in here if it has one
+    $mail->DKIM_passphrase = '';
+    //The identity you're signing as - usually your From address
+    $mail->DKIM_identity = $mail->From;
+    if ($email['txt_message'] != '') {
+      $mail->AltBody = $email['txt_message'];
+    }
+    if ($mail->send()) {
+      // $stmt = $db->prepare('DELETE FROM outbox WHERE id = ?');
+      // $stmt->execute(array($email['id']));
+    } else {
+      echo $mail->ErrorInfo;
     }
   }
 }
