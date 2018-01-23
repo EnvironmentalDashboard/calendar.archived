da20de52b5dccab039d4f2c7ee2c895187144514
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-01-23 11:24:22 -0500

    temp fix for prefs

--- prefs/includes/check-signed-in.php
+++ prefs/includes/check-signed-in.php
@@ -1,6 +1,7 @@
 <?php 
 // Assumes db.php already imported
-$symlink = explode('/', $_SERVER['REQUEST_URI'])[1];
+// $symlink = explode('/', $_SERVER['REQUEST_URI'])[1];
+$symlink = 'oberlin';
 $stmt = $db->prepare('SELECT token FROM users WHERE slug = ?');
 $stmt->execute(array($symlink));
 if (!isset($_COOKIE['token']) || $stmt->fetchColumn() !== $_COOKIE['token']) {
