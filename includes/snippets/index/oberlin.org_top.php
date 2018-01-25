7c69d969bd7a6593613d5f6910dfc64ca7474ce1
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-01-24 23:44:44 -0500

    embed for oberlin.org

---
+++ includes/snippets/index/oberlin.org_top.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Community Events Calendar</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.css?<?php echo time(); ?>">
+    <style>
+      /*@media (max-width: 950px) {*/
+      @media (max-width: 768px) {
+        .hidden-sm-down {display: none;}
+      }
+      @media (max-width: 990px) {
+        .hidden-md-down {display: none;}
+      }
+      .bg-primary, .bg-dark {color:#fff;}
+      td.day {border: 1px solid #eee}
+      table {table-layout: fixed;width: 100%}
+    </style>
+  </head>
+  <body>
+    <div class="container">
