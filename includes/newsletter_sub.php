e2e5aa5ec826001d8e84df68f22ed6e8b012031c
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-02-09 11:47:12 -0500

    new email

--- includes/newsletter_sub.php
+++ includes/newsletter_sub.php
@@ -4,6 +4,6 @@ if (isset($_POST['email'])) {
   $stmt = $db->prepare('REPLACE INTO newsletter_recipients (email) VALUES (?)');
   $stmt->execute(array($_POST['email']));
   $stmt = $db->prepare('INSERT INTO outbox (recipient, subject, txt_message, html_message) VALUES (?, ?, ?, ?)');
-  $stmt->execute(array($_POST['email'], "Subscription Confirmation", "<p style='padding:30px'>You're subscribed to our newsletter, but you must enable HTML emails to read them.", "You're subscribed to our weekly newsletter. You can unsubscribe any time by clicking <a href='https://environmentaldashboard.org/calendar/unsubscribe?email={$_POST['email']}'>here</a> or any of the unsubscribe links included in each newsletter.</p>"));
+  $stmt->execute(array($_POST['email'], "Subscription Confirmation", "You're subscribed to our newsletter, but you must enable HTML emails to read them.", "<div style='padding:30px'><h1>You're subscribed!</h1> <p>Every Friday, we will send you a newsletter detailing events in the community. You can customize the types of events included in your newsetter <a href='https://environmentaldashboard.org/calendar/customize-sub?email={$_POST['email']}'>here</a>. You can unsubscribe any time by clicking <a href='https://environmentaldashboard.org/calendar/unsubscribe?email={$_POST['email']}'>here</a> or any of the unsubscribe links included in each newsletter.</p></div>"));
 }
 ?>
