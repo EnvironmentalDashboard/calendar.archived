d4145c4e7f70648b8678285f9e75880f85cf8c18
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-02-18 18:38:17 -0500

    bug

--- includes/load_calendar.php
+++ includes/load_calendar.php
@@ -4,6 +4,8 @@ ini_set('display_errors', 'On');
 date_default_timezone_set('America/New_York');
 require '../../includes/db.php';
 require '../includes/class.CalendarHTML.php';
+require '../includes/class.CalendarRoutes.php';
+$router = new CalendarRoutes($_SERVER['SCRIPT_FILENAME']);
 if (isset($_GET['month']) && isset($_GET['year'])) {
   $start_time = strtotime("{$_GET['year']}-{$_GET['month']}-01 00:00:00");
   $end_time = strtotime("{$_GET['year']}-{$_GET['month']}-".cal_days_in_month(CAL_GREGORIAN, $_GET['month'], $_GET['year'])." 00:00:00");
@@ -15,5 +17,5 @@ $cal = new CalendarHTML($db);
 $cal->set_start($start_time);
 $cal->set_end($end_time);
 $cal->fetch_events();
-$cal->print_cal();
+$cal->print_cal($router);
 ?>
