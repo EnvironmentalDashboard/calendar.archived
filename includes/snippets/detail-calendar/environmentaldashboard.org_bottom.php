8b22d7cd94c84e9c1c6999126c3965cf3a163ccf
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-03-20 16:18:50 -0500

    forgot to copy over

--- includes/snippets/detail-calendar/environmentaldashboard.org_bottom.php
+++ includes/snippets/detail-calendar/environmentaldashboard.org_bottom.php
@@ -2,10 +2,18 @@
 <?php include dirname(dirname($_SERVER['SCRIPT_FILENAME'])).'/includes/footer.php'; ?>
 </div> <!-- /.container -->
     <?php include dirname(dirname($_SERVER['SCRIPT_FILENAME'])).'/includes/js.php'; ?>
-    <!-- <script>
-      $(function () {
-        $('[data-toggle="popover"]').popover({ trigger: "hover" });
-      });
-    </script> -->
+    <script>
+      $('#eventModal').on('show.bs.modal', function (event) {
+      	var dest = $('#modal-body');
+      	dest.html('');
+			  var button = $(event.relatedTarget)
+			  var ids = button.data('ids');
+			  var titles = JSON.parse(button.attr('data-titles'));
+			  var descripts = JSON.parse(button.attr('data-descripts'));
+			  for (var i=0; i < ids.length; i++) { 
+			  	dest.append('<h4>'+titles[i]+'</h4>'+'<p>'+descripts[i]+'</p><p><a class="btn btn-primary btn-sm" href="<?php echo "{$router->base_url}/calendar/detail{$router->detail_page_sep}"; ?>'+ids[i]+'">View more</a></p>');
+			  }
+			})
+    </script>
   </body>
 </html>
