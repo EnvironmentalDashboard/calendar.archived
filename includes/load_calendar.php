479617253e197bc3cfe3f7d771d9b0c11000ff07
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2018-09-25 17:20:27 -0400

    oops

--- includes/load_calendar.php
+++ includes/load_calendar.php
@@ -8,7 +8,7 @@ require '../includes/class.CalendarRoutes.php';
 $router = new CalendarRoutes($_SERVER['SCRIPT_FILENAME']);
 if (isset($_GET['month']) && isset($_GET['year'])) {
   $start_time = strtotime("{$_GET['year']}-{$_GET['month']}-01 00:00:00");
-  $end_time = strtotime("{$_GET['year']}-{$_GET['month']}-".cal_days_in_month(CAL_GREGORIAN, $_GET['month'], $_GET['year'])." 00:00:00");
+  $end_time = strtotime("{$_GET['year']}-{$_GET['month']}-".cal_days_in_month(CAL_GREGORIAN, $_GET['month'], $_GET['year'])." 24:00:00");
 } else {
   $start_time = strtotime(date('Y-m-') . "01 00:00:00"); // Start of the month
   $end_time = strtotime(date('Y-m-t') . " 24:00:00"); // End of the month
