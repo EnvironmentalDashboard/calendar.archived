70e190c4105883bfdca324b8bfb97cc5e73d7032
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-01-30 11:56:51 -0500

    restart policy

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 docker build -t calendar .
 server=$(hostname -A 2>/dev/null)
 if [ $server == "environmentaldashboard.org" ]; then
-  docker run -dit -p 4000:80 -e COMMUNITY="" --name oberlin-calendar \
+  docker run -dit -p 4000:80 -e COMMUNITY="" --name oberlin-calendar --restart always \
     -v /etc/opendkim/keys/environmentaldashboard.org/mail.private:/opendkim/mail.private \
     -v /var/www/uploads/calendar:/var/www/uploads/calendar \
     calendar
-  docker run -dit -p 4001:80 -e COMMUNITY="obp" --name obp-calendar \
+  docker run -dit -p 4001:80 -e COMMUNITY="obp" --name obp-calendar --restart always \
     -v /var/www/uploads/calendar:/var/www/uploads/calendar \
     calendar
-  docker run -dit -p 4002:80 -e COMMUNITY="cleveland" --name cleveland-calendar \
+  docker run -dit -p 4002:80 -e COMMUNITY="cleveland" --name cleveland-calendar --restart always \
     -v /var/www/uploads/calendar:/var/www/uploads/calendar \
     calendar
 else
