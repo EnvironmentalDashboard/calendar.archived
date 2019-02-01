f2f99ed3ed6665719fdea03f95a2b596bba08eda
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-02-01 15:38:30 -0500

    more link fixes

--- slide3.php
+++ slide3.php
@@ -2,7 +2,7 @@
 error_reporting(-1);
 ini_set('display_errors', 'On');
 date_default_timezone_set('America/New_York');
-require '../includes/db.php';
+require 'includes/db.php';
 $stmt = $db->prepare('SELECT id, event, start, `end`, description, loc_id, has_img, no_start_time, no_end_time, event_type_id FROM calendar WHERE id = ? LIMIT 1');
 $stmt->execute(array($_GET['id']));
 if ($stmt->rowCount() === 0) {
