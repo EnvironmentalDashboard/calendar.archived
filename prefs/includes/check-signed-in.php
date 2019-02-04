accb587a230375b338e7c027ebf3b88c15532169
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-02-04 12:09:04 -0500

    oberlin is default

--- prefs/includes/check-signed-in.php
+++ prefs/includes/check-signed-in.php
@@ -1,8 +1,9 @@
 <?php 
 // Assumes db.php already imported
 $stmt = $db->prepare('SELECT token FROM users WHERE slug = ?');
-$stmt->execute([$community]);
+$stmt->execute([($community == '') ? 'oberlin' : $community]);
 if (!isset($_COOKIE['token']) || $stmt->rowCount() === 0 || $stmt->fetchColumn() !== $_COOKIE['token']) {
   header("Location: https://{$community}.environmentaldashboard.org/calendar/prefs/");
+  exit;
 }
 ?>
