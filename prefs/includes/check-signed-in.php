23fd9914de89903326483d93456b760698fe10ee
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-02-04 12:20:12 -0500

    oops actually this is the fix

--- prefs/includes/check-signed-in.php
+++ prefs/includes/check-signed-in.php
@@ -1,9 +1,11 @@
 <?php 
 // Assumes db.php already imported
+if ($community == '') {
+  $community = 'oberlin'; // default to oberlin
+}
 $stmt = $db->prepare('SELECT token FROM users WHERE slug = ?');
-$stmt->execute([($community == '') ? 'oberlin' : $community]);
+$stmt->execute([$community]);
 if (!isset($_COOKIE['token']) || $stmt->rowCount() === 0 || $stmt->fetchColumn() !== $_COOKIE['token']) {
   header("Location: https://{$community}.environmentaldashboard.org/calendar/prefs/");
   exit;
 }
-?>
