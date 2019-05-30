7964e2e676be46d849b81630d307148b29daf8da
Tim Robert-Fitzgerald <tim.terf@gmail.com>
2019-05-30 08:24:39 -0400

    increased max upload size

--- build/build.sh
+++ build/build.sh
@@ -5,5 +5,11 @@ if [ -z "${COMMUNITY}" ] || [ "${COMMUNITY}" = "oberlin" ]; then
 else
   mv /var/www/html/includes/snippets/${COMMUNITY}/*.php /var/www/html/includes/snippets/
 fi
+
 mv /var/www/html/apache/http.conf /etc/apache2/sites-available/000-default.conf
+
 ln -snf /var/www/uploads/calendar /var/www/html/images/uploads
+
+INI_LOC=`php -i | grep 'Loaded Configuration File => ' | sed 's/Loaded Configuration File => //g' | sed 's/cli/apache2/g'`
+sed -ie 's/upload_max_filesize = 2M/upload_max_filesize = 64M/g' "$INI_LOC"
+sed -ie 's/post_max_size = 8M/post_max_size = 512M/g' "$INI_LOC"
